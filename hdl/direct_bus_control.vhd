58f3f49e274f8974c9c734031af601d7a8899c11
SkyeErnst <SkyeErnst97@gmail.com>
2020-08-19 16:25:58 -0400

    Removed PLL module due to errors on 2nd dev station.
    continued development of direct_bus_control

--- hdl/direct_bus_control.vhd
+++ hdl/direct_bus_control.vhd
@@ -4,19 +4,33 @@ use IEEE.numeric_std.all;
 
 entity direct_bus_control is
     port (
-        i_clk           : in std_logic := '0';
-        i_mod_en        : in std_logic := '1';
-        i_enable_in     : in std_logic := '1';
-        i_enable_out    : in std_logic := '1';
-        i_switches      : in std_logic_vector(7 downto 0) := (others => '0');
+        i_clk                   : in std_logic := '0';
+        i_rst                   : in std_logic := '1';
+        i_module_en             : in std_logic := '1';
+        i_write_enable          : in std_logic := '1'; -- 1 = implicit read enable
+        i_switches              : in std_logic_vector(7 downto 0) := (others => '0');
 
+        o_databus               : out std_logic_vector(7 downto 0) := (others => 'Z')
     );
 end entity direct_bus_control;
 
 architecture rtl of direct_bus_control is
-    
+    signal q : std_logic_vector(7 downto 0) := (others => 'Z');
 begin
     
-    
+    proc_name: process(i_clk, i_rst)
+    begin
+        if i_rst = '0' then
+            q <= (others => 'Z');
+        elsif rising_edge(i_clk) then
+            if (i_module_en = '0' AND i_rst = '1') then
+                if (i_write_enable = '1') then -- 'read' condition
+                    q <= i_switches;
+                else
+                    o_databus <= q;
+                end if;
+            end if;
+        end if;
+    end process proc_name;
     
 end architecture rtl;
--- synthesys/simulation/modelsim/Nx8_iputf_input/mentor/msim_setup.tcl
+++
@@ -1,3 +0,0 @@
-
-
-vcom "F:/_PROGRAMING/VHDL/Nighthawk/Nx8/hdl/pll_system/pllsystem_sim/pllsystem.vho"
